1fff3be3419714ff2b52e6e3d6dcc0c90ae20309
Mkaligraphics <nkhalammar@gmail.com>
2023-02-17 23:01:17 +0300

    0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	return (0);
+}#include <stdlib.h>
+  2 #include <time.h>
+  3 /* more headers goes there */
+  4 
+  5 /* betty style doc for function main goes there */
+  6 int main(void)
+  7 {
+  8         int n;
+  9 
+ 10         srand(time(0));
+ 11         n = rand() - RAND_MAX / 2;
+ 12         /* your code goes there */
+ 13         return (0);
+ 14 }
+~         
