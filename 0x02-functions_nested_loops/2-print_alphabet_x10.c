922f2fc1bc1a8b3bdafb35540f60dc37b4795b78
Mkaligraphics <nkhalammar@gmail.com>
2023-02-21 14:28:40 +0300

    print_alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet in lowercase, followed by a new line
+ *
+ * Return: 0
+*/
+void print_alphabet_x10(void)
+
+{
+
+	int x = 0;
+
+	while (x <= 10);
+	{
+        x++;
+	_putchar(x);
+	}
+	_putchar('\n');
+
+    return (0);
+}
