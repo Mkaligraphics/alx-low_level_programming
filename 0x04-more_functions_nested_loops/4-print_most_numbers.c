151a70661b5f8869c3811f70d4655dfd439a1104
Mkaligraphics <nkhalammar@gmail.com>
2023-02-23 22:36:38 +0300

    Task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,10 +7,8 @@ void print_most_numbers(void)
     int i;
 for (i = 0; i<=9; i++)
 {
-    if (i == 2 || i == 4)
-    {
-        continue;
-    }
+    if (i != 2 && i != 4)
+			_putchar((i % 10) + '0');
     _putchar(i);
 }
     _putchar('\n');
